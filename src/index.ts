57cf5f6f2a2ace5a60de4e116d0f0ae35101bf07
Carlos Precioso <kalprestito@gmail.com>
2020-04-24 19:55:42 +0200

    Export Alignment and Origin enums

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { BlockLayoutDrawOptions, drawBlockLayout } from "./draw/block"
+export { Alignment, Origin } from "./draw/common"
 export { drawLineLayout, LineLayoutDrawOptions } from "./draw/line"
 export { BlockLayout, layoutBlock } from "./layout/block"
 export { layoutLine, LineLayout } from "./layout/line"
